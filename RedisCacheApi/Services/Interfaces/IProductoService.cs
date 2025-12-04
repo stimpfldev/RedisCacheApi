b44cecad1b95425b64dce7b267187b8034163d56
Federico Stimpfl <stimpfldev@gmail.com>
2025-12-04 20:42:00 -0300

    Update comments in IProductoService interface
    Refactor comments to improve clarity and maintain consistency.

--- RedisCacheApi/Services/Interfaces/IProductoService.cs
+++ RedisCacheApi/Services/Interfaces/IProductoService.cs
@@ -1,13 +1,10 @@
-﻿//IProductoService cumple 3 roles 
-//1 Define el contrato del servicio (qué debe hacer, no cómo)
-//2 Permite cambiar la implementación sin romper nada (desacoplamiento)
-//3 Hace posible el Decorator Pattern (que vas a usar con Redis)
+
 using RedisCacheApi.Models;
 
 namespace RedisCacheApi.Services.Interfaces;
 
-// 👉 Este contrato define lo que ofrece el servicio de productos.
-//    Redis se integrará SIN tocar estos métodos (decorator), manteniendo desacoplamiento.
+// contrato q define lo que ofrece el servicio de productos.
+//    Redis se integrará son tocar estos métodos (decorator), asi mantengo  desacoplamiento.
 public interface IProductoService
 {
     List<Producto> GetAll();
