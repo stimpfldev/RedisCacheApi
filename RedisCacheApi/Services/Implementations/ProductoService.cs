d0eb1ad3483f122ed477da59e2a4e7824221eeed
Federico Stimpfl <stimpfldev@gmail.com>
2025-12-04 20:41:03 -0300

    Refactor logging messages in ProductoService

--- RedisCacheApi/Services/Implementations/ProductoService.cs
+++ RedisCacheApi/Services/Implementations/ProductoService.cs
@@ -3,24 +3,23 @@ using RedisCacheApi.Services.Interfaces;
 
 namespace RedisCacheApi.Services.Implementations;
 
-// 👉 ESTE es el servicio REAL (sin caché). 
-//    Es limpio, simple, y representa la "lógica de negocio" real.
-//    Redis se agregará DESPUÉS con un Decorator.
+// servicio REAL (sin caché). 
+
 public class ProductoService : IProductoService
 {
-    // 👉 Lista en memoria simulando base de datos
+    //  Lista en memoria simulando base de datos
     private readonly List<Producto> _productos = new();
     private int _nextId = 1;
 
     public List<Producto> GetAll()
     {
-        Console.WriteLine("➡️ ProductoService.GetAll ejecutado (NO usa cache)");
+        Console.WriteLine(" ProductoService.GetAll ejecutado (NO usa cache)");
         return _productos.ToList();
     }
 
     public Producto GetById(int id)
     {
-        Console.WriteLine($"➡️ ProductoService.GetById({id}) ejecutado (NO usa cache)");
+        Console.WriteLine($" ProductoService.GetById({id}) ejecutado (NO usa cache)");
         return _productos.FirstOrDefault(p => p.Id == id);
     }
 
@@ -34,7 +33,7 @@ public class ProductoService : IProductoService
 
         _productos.Add(nuevo);
 
-        Console.WriteLine("🟡 ProductoService.Create ejecutado (NO usa cache)");
+        Console.WriteLine(" ProductoService.Create ejecutado (NO usa cache)");
 
         return nuevo;
     }
